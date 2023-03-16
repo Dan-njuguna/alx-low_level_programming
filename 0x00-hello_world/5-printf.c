f0ccc992c6f9038b31235868e152793cc3ba769d
La Techist <njugush020@gmail.com>
2023-03-16 03:08:23 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,6 @@
  */
 int main(void)
 {
-	/*The program prints exactly the given line and a new line at the end*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
