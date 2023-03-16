8f80c11edd478ad2b53fc7def3bbf26f82498dd6
La Techist <njugush020@gmail.com>
2023-03-16 02:46:12 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry
- *
  * Return: Always 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle," \n);
+	/*The program prints exactly the given line and leaves a new line*/
+	puts("Programming is like building a multilingual puzzle,");
 	return (0);
 }
