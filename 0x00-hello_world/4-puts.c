7096dbe9cac0a32df1996b24ddca2266d1aa36ed
La Techist <njugush020@gmail.com>
2023-03-16 02:47:25 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
 int main(void)
 {
 	/*The program prints exactly the given line and leaves a new line*/
-	puts("Programming is like building a multilingual puzzle,");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
