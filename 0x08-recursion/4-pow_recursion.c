bf4180a0062e989ede515b8cb282dc6317956912
La Techist <njugush020@gmail.com>
2023-04-06 22:04:18 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "stdlib.h"
+#include "time.h"
+#include "stdio.h"
+/**
+ * _pow_recursion - computes the value of x raised to power y
+ * @x: Base integer
+ * @y: Index integer
+ * Return: Integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return(-1);
+	else if (y == 0)
+		return(1);
+	else
+		--y;
+	return(x * _pow_recursion(x, y));
+}
