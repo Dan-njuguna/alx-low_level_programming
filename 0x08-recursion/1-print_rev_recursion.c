c0aec7b0f2c4c2661d18aed4e8ed5cf22be1ad54
La Techist <njugush020@gmail.com>
2023-04-06 21:47:08 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "time.h"
 /**
  * _print_rev_recursion - reverse string
  * @s: pointer string
@@ -9,6 +12,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		putchar(*s);
 	}
 }
