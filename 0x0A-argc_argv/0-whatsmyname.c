f2013129158e74cf4645c6ab3932872996d66c12
La Techist <njugush020@gmail.com>
2023-04-11 05:04:12 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	while (argc--)
 		printf("%s\n", *argv++);
-	return (0);
+	exit(EXIT_SUCCESS);
 }
