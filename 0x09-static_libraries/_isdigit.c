1a52d4e1a3a03a82b98e696b2b0f5be21f2fc0e8
Dan Njuguna <126158747+Dan-njuguna@users.noreply.github.com>
2023-04-19 09:44:34 +0300

    Delete 0x09-static_libraries directory

--- 0x09-static_libraries/_isdigit.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-int _isdigit(int c)
-{
-	/* implementation of _isdigit */
-}
--- 0x09-static_libraries/_putchar.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * _putchar - prints one string only
- * @c: a character
- */
-int _putchar(char c)
-{
-	/* Implementation of _putchar */
-}
