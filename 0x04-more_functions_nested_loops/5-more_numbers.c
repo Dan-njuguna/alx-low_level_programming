4c5c699a091200f9b5d6e9b6c23f7fad9236e3b6
Dan-njuguna <njugush020@gmail.com>
2023-05-30 09:21:41 +0300

    6-print_lines.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,15 @@ void more_numbers(void)
 	{
 		for (n = 0; n <= 14; n++)
 		{
-			if (n > 0)
+			if (n < 10)
+			{
+				putchar(n + '0');
+			}
+			else
 			{
 				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
 			}
-			putchar((n % 10) + '0');
 		}
 		putchar('\n');
 	}
