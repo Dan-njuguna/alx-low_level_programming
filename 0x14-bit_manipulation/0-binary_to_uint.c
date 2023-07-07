59f3803461af78079a15ede95bdee536268d54ec
Dan-njuguna <njugush020@gmail.com>
2023-07-07 10:08:26 +0300

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,20 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n;
-	int i;
+	int i = 0;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	for (i = 0; b[i]; i++)
+	while (b[i])
 	{
 		if (b[i] < '0' || b[i] > '1')
 		{
 			return (0);
 		}
 		n =(n * 2) + (*b - '0');
+		i++;
 	}
 	return (n);
 }
